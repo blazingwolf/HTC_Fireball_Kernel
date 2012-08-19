db684e5df4787a14e9749afdf934d2bfd04f6620
blazingwolf <blazingwolf@outlook.com>
2012-08-19 16:08:21 -0400

    add overclock

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Sun Aug 19 13:47:31 EDT 2012"
+#define LINUX_COMPILE_BY "blazingwolf"
+#define LINUX_COMPILE_HOST "blazingwolf-Dell-XPS-L502X"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.8-g8508293-dirty"
